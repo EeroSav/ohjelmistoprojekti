6402f2da8c463806df8679449a5401387b3a1124
EeroSav <eero.sav@gmail.com>
2022-04-04 18:36:38 +0300

    first commit

---
+++ src/main/java/com/example/ohjelmistoprojekti/OhjelmistoprojektiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ohjelmistoprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhjelmistoprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhjelmistoprojektiApplication.class, args);
+	}
+
+}
