5b0406842fa698621a897f614956bcf6afa2a920
tukiainenm <miromatias@hotmail.com>
2022-04-12 13:05:24 +0300

    addQuestion lisätty controlleriin

--- src/main/java/com/example/ohjelmistoprojekti/web/kyselyController.java
+++ src/main/java/com/example/ohjelmistoprojekti/web/kyselyController.java
@@ -2,10 +2,15 @@ package com.example.ohjelmistoprojekti.web;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class kyselyController {
 
-
-
+    @RequestMapping(value = "/addQuestion")
+    public String addQuestion(Model model) {
+        model.addAttribute("question", new Kysymys());
+        return "addQuestion";
+    }
 }
