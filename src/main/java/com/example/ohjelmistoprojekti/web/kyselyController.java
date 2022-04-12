a7a3e8ae88fdf32ca413f961246c025748b2a291
EeroSav <eero.sav@gmail.com>
2022-04-12 13:01:22 +0300

    päivitetty pom.xml, lisätty kysymys constructor

--- src/main/java/com/example/ohjelmistoprojekti/web/kyselyController.java
+++ src/main/java/com/example/ohjelmistoprojekti/web/kyselyController.java
@@ -2,10 +2,9 @@ package com.example.ohjelmistoprojekti.web;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class kyselyController {
-
-
 
 }
