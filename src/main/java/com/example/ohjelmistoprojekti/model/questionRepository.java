142eea4c117f12b4c4137671ca90b522d0465bb6
tukiainenm <83721917+tukiainenm@users.noreply.github.com>
2022-04-14 10:24:39 +0300

    Html-templatejen otsikot vaihdettu, questionrepository muokattu

--- src/main/java/com/example/ohjelmistoprojekti/model/questionRepository.java
+++ src/main/java/com/example/ohjelmistoprojekti/model/questionRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface questionRepository extends CrudRepository<Question, Long> {
+
     List<Question> findByTitle(String title);
 }
