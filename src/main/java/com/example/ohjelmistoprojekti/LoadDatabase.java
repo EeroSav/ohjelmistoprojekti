24d41389d6ddaeca8aeb3ced19d5bfffab95b8e0
Eero Savolainen <eero.sav@gmail.com>
2022-05-04 22:32:21 +0300

    Lisätty security

--- src/main/java/com/example/ohjelmistoprojekti/LoadDatabase.java
+++ src/main/java/com/example/ohjelmistoprojekti/LoadDatabase.java
@@ -1,9 +1,6 @@
 package com.example.ohjelmistoprojekti;
 
-import com.example.ohjelmistoprojekti.model.Answer;
-import com.example.ohjelmistoprojekti.model.AnswerRepository;
-import com.example.ohjelmistoprojekti.model.Question;
-import com.example.ohjelmistoprojekti.model.QuestionRepository;
+import com.example.ohjelmistoprojekti.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -15,7 +12,7 @@ class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
-    CommandLineRunner questionDemo(QuestionRepository qrepository, AnswerRepository arepository) {
+    CommandLineRunner questionDemo(QuestionRepository qrepository, AnswerRepository arepository, UserRepository urepository) {
         return args -> {
             log.info("save examplequestions");
 
@@ -26,6 +23,11 @@ class LoadDatabase {
             arepository.save(new Answer("asd", qrepository.findByTitle("kysymys").get(0)));
             arepository.save(new Answer("asas", qrepository.findByTitle("kysymys2").get(0)));
 
+            User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "mallispo@asd", "USER");
+            User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "mallispo2@asd", "ADMIN");
+
+            urepository.save(user1);
+            urepository.save(user2);
 
             log.info("fetch all questions");
             for (Question question : qrepository.findAll()){
